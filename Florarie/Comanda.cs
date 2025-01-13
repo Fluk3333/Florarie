64f3283f6507d576c64ce2b1e6469be84c7705da
Petre <barboipetre12@gmail.com>
2025-01-13 13:42:32 +0200

    Am creat clasa Review si ComandaMaterie

---
+++ Florarie/Comanda.cs
@@ -0,0 +1,19 @@
+﻿namespace Florarie;
+public enum StatusComandaBuchet
+{
+    InPreluare,
+    AsteptareMaterie,
+    InLucru,
+    Finalizat,
+    Revendicat
+    
+}
+public class Comanda
+{
+    public string CodUnic { get; set; }
+    public Client Client { get; set; }
+    public string Nume { get; set; }
+    public string Telefon { get; set; }
+    public string Descriere { get; set; }
+    public StatusComandaBuchet Status { get; set; }
+}
---
+++ Florarie/ComandaMaterie.cs
@@ -0,0 +1,13 @@
+﻿namespace Florarie;
+public enum StatusComanda
+{
+    InAsteptare,
+    Finalizat
+}
+public class ComandaMaterie
+{
+    public string CodUnic { get; set; }
+    public string Descriere { get; set; }
+    public string CodUnicMaterie { get; set; }
+    public StatusComanda Status { get; set; }
+}
